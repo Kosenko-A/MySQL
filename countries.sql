3a3f9279b0a9921b05c00c5ed4d9bed2c4871335
Angelina Kosenko <67165094+Kosenko-A@users.noreply.github.com>
2020-11-02 16:30:23 +0500

    Homework 2 lesson

---
+++ countries.sql
@@ -0,0 +1,20 @@
+SELECT * FROM geodata._countries;
+ALTER TABLE `geodata`.`_countries` 
+DROP COLUMN `title_cz`,
+DROP COLUMN `title_lv`,
+DROP COLUMN `title_lt`,
+DROP COLUMN `title_ja`,
+DROP COLUMN `title_pl`,
+DROP COLUMN `title_it`,
+DROP COLUMN `title_fr`,
+DROP COLUMN `title_de`,
+DROP COLUMN `title_pt`,
+DROP COLUMN `title_es`,
+DROP COLUMN `title_en`,
+DROP COLUMN `title_be`,
+DROP COLUMN `title_ua`,
+CHANGE COLUMN `country_id` `country_id` INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN `title_ru` `title` VARCHAR(150) NOT NULL ,
+ADD PRIMARY KEY (`country_id`),
+ADD INDEX `title_id` (`title` ASC) VISIBLE;
+;
